658a233d9d37582f62776f3e5a15d5b19b569723
Gopichand Guguloth <gopichandguguloth.123@gmail.com>
2026-03-25 22:22:29 +0530

    fix:table bugs

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -13,9 +13,9 @@ export default async function RootLayout({ children }) {
       <div className="flex-1 flex flex-col px-4 pb-4">
         <div
           className="absolute inset-0 -z-10 h-full w-full bg-background 
-          dark:bg-[radial-gradient(#393e4a_1px,transparent_1px)] 
+          ``dark:bg-[radial-gradient(#393e4a_1px,transparent_1px)] 
           dark:bg-[size:16px_16px] 
-          bg-[radial-gradient(#dadde2_1px,transparent_1px)] 
+          bg-[radial-gradient(#dadde2_1px,transparent_1px)] ``
           bg-[size:16px_16px]"
         />
 
